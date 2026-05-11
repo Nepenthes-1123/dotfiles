cc5627dbfd319c9044decc1e3bf4029b3e03343b
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-05-12 00:58:57 +0900

    fix: update keybindings for tab activation and add backslash handling

--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -4,6 +4,7 @@ source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $HOME/.zsh/fzf-tab/fzf-tab.plugin.zsh
 
 ### other-plugins
+[ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh
 source <(fzf --zsh)
 
 #### zsh-autocompleteと組み合わせる場合、いい感じに補完をするために以下の設定を追加
