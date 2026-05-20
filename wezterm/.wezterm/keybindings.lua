870e0908612c6d7d3fa2f3c6128a16b9a9fb5319
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-05-21 07:57:30 +0900

    fix: update keybindings for leader key and adjust font size shortcuts

--- wezterm/.wezterm/keybindings.lua
+++ wezterm/.wezterm/keybindings.lua
@@ -2,125 +2,66 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 
 return {
-  leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 },
+  leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 2000 },
   keys = {
     { key = 'Tab', mods = 'CTRL', action = act.ActivateTabRelative(1) },
     { key = 'Tab', mods = 'SHIFT|CTRL', action = act.ActivateTabRelative(-1) },
     { key = 'Enter', mods = 'ALT', action = act.ToggleFullScreen },
-    { key = '+', mods = 'CTRL', action = act.IncreaseFontSize },
-    { key = '+', mods = 'SHIFT|CTRL', action = act.IncreaseFontSize },
-    { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
-    { key = '-', mods = 'SHIFT|CTRL', action = act.DecreaseFontSize },
-    { key = '-', mods = 'SUPER', action = act.DecreaseFontSize },
+    { key = '<', mods = 'CTRL|SHIFT', action = act.DecreaseFontSize },
+    { key = '>', mods = 'CTRL|SHIFT', action = act.IncreaseFontSize },
     { key = '0', mods = 'CTRL', action = act.ResetFontSize },
-    { key = '0', mods = 'SHIFT|CTRL', action = act.ResetFontSize },
-    { key = '0', mods = 'SUPER', action = act.ResetFontSize },
-    { key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
-    { key = '=', mods = 'SHIFT|CTRL', action = act.IncreaseFontSize },
-    { key = '=', mods = 'SUPER', action = act.IncreaseFontSize },
-    { key = 'C', mods = 'CTRL', action = act.CopyTo 'Clipboard' },
     { key = 'C', mods = 'SHIFT|CTRL', action = act.CopyTo 'Clipboard' },
-    { key = 'F', mods = 'CTRL', action = act.Search 'CurrentSelectionOrEmptyString' },
+    { key = 'c', mods = 'SUPER', action = act.CopyTo 'Clipboard' },
     { key = 'F', mods = 'SHIFT|CTRL', action = act.Search 'CurrentSelectionOrEmptyString' },
-    { key = 'K', mods = 'CTRL', action = act.ClearScrollback 'ScrollbackOnly' },
+    { key = 'f', mods = 'SUPER', action = act.Search 'CurrentSelectionOrEmptyString' },
     { key = 'K', mods = 'SHIFT|CTRL', action = act.ClearScrollback 'ScrollbackOnly' },
-    { key = 'L', mods = 'CTRL', action = act.ShowDebugOverlay },
+    { key = 'k', mods = 'SUPER', action = act.ClearScrollback 'ScrollbackOnly' },
     { key = 'L', mods = 'SHIFT|CTRL', action = act.ShowDebugOverlay },
-    { key = 'M', mods = 'CTRL', action = act.Hide },
+    { key = 'l', mods = 'SUPER', action = act.ShowDebugOverlay },
     { key = 'M', mods = 'SHIFT|CTRL', action = act.Hide },
-    { key = 'N', mods = 'CTRL', action = act.SpawnWindow },
+    { key = 'm', mods = 'SUPER', action = act.Hide },
     { key = 'N', mods = 'SHIFT|CTRL', action = act.SpawnWindow },
-    { key = 'P', mods = 'CTRL', action = act.ActivateCommandPalette },
+    { key = 'n', mods = 'SUPER', action = act.SpawnWindow },
     { key = 'P', mods = 'SHIFT|CTRL', action = act.ActivateCommandPalette },
-    { key = 'R', mods = 'CTRL', action = act.ReloadConfiguration },
+    { key = 'p', mods = 'SUPER', action = act.ActivateCommandPalette },
     { key = 'R', mods = 'SHIFT|CTRL', action = act.ReloadConfiguration },
-    { key = 'T', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
-    { key = 'T', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain'
-},
-    { key = 'U', mods = 'CTRL', action = act.CharSelect{ copy_on_select = true,
-copy_to =  'ClipboardAndPrimarySelection' } },
+    { key = 'r', mods = 'SUPER', action = act.ReloadConfiguration },
+    { key = 'T', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 't', mods = 'SUPER', action = act.SpawnTab 'CurrentPaneDomain' },
     { key = 'U', mods = 'SHIFT|CTRL', action = act.CharSelect{ copy_on_select =
 true, copy_to =  'ClipboardAndPrimarySelection' } },
-    { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-    { key = 'V', mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
-    { key = 'W', mods = 'CTRL', action = act.CloseCurrentTab{ confirm = true } },
-    { key = 'W', mods = 'SHIFT|CTRL', action = act.CloseCurrentTab{ confirm = true } },
-    { key = 'X', mods = 'CTRL', action = act.ActivateCopyMode },
-    { key = 'X', mods = 'SHIFT|CTRL', action = act.ActivateCopyMode },
-    { key = 'Z', mods = 'CTRL', action = act.TogglePaneZoomState },
-    { key = 'Z', mods = 'SHIFT|CTRL', action = act.TogglePaneZoomState },
-    { key = '[', mods = 'SHIFT|SUPER', action = act.ActivateTabRelative(-1) },
-    { key = ']', mods = 'SHIFT|SUPER', action = act.ActivateTabRelative(1) },
-    { key = '_', mods = 'CTRL', action = act.DecreaseFontSize },
-    { key = '_', mods = 'SHIFT|CTRL', action = act.DecreaseFontSize },
-    { key = 'c', mods = 'SHIFT|CTRL', action = act.CopyTo 'Clipboard' },
-    { key = 'c', mods = 'SUPER', action = act.CopyTo 'Clipboard' },
-    { key = 'f', mods = 'SHIFT|CTRL', action = act.Search 'CurrentSelectionOrEmptyString' },
-    { key = 'f', mods = 'SUPER', action = act.Search 'CurrentSelectionOrEmptyString' },
-    { key = 'k', mods = 'SHIFT|CTRL', action = act.ClearScrollback 'ScrollbackOnly' },
-    { key = 'k', mods = 'SUPER', action = act.ClearScrollback 'ScrollbackOnly' },
-    { key = 'l', mods = 'SHIFT|CTRL', action = act.ShowDebugOverlay },
-    { key = 'm', mods = 'SHIFT|CTRL', action = act.Hide },
-    { key = 'm', mods = 'SUPER', action = act.Hide },
-    { key = 'n', mods = 'SHIFT|CTRL', action = act.SpawnWindow },
-    { key = 'n', mods = 'SUPER', action = act.SpawnWindow },
-    { key = 'p', mods = 'SHIFT|CTRL', action = act.ActivateCommandPalette },
-    { key = 'r', mods = 'SHIFT|CTRL', action = act.ReloadConfiguration },
-    { key = 'r', mods = 'SUPER', action = act.ReloadConfiguration },
-    { key = 't', mods = 'SHIFT|CTRL', action = act.SpawnTab 'CurrentPaneDomain'
-},
-    { key = 't', mods = 'SUPER', action = act.SpawnTab 'CurrentPaneDomain' },
     { key = 'u', mods = 'SHIFT|CTRL', action = act.CharSelect{ copy_on_select =
 true, copy_to =  'ClipboardAndPrimarySelection' } },
-    { key = 'v', mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
+    { key = 'V', mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
     { key = 'v', mods = 'SUPER', action = act.PasteFrom 'Clipboard' },
-    { key = 'w', mods = 'SHIFT|CTRL', action = act.CloseCurrentTab{ confirm = true } },
-    { key = 'w', mods = 'SUPER', action = act.CloseCurrentTab{ confirm = true }
-},
-    { key = 'x', mods = 'SHIFT|CTRL', action = act.ActivateCopyMode },
-    { key = 'z', mods = 'SHIFT|CTRL', action = act.TogglePaneZoomState },
-    { key = '{', mods = 'SUPER', action = act.ActivateTabRelative(-1) },
-    { key = '{', mods = 'SHIFT|SUPER', action = act.ActivateTabRelative(-1) },
-    { key = '}', mods = 'SUPER', action = act.ActivateTabRelative(1) },
-    { key = '}', mods = 'SHIFT|SUPER', action = act.ActivateTabRelative(1) },
+    { key = 'W', mods = 'SHIFT|CTRL', action = act.CloseCurrentTab{ confirm = true } },
+    { key = 'w', mods = 'SUPER', action = act.CloseCurrentTab{ confirm = true } },
+    { key = 'X', mods = 'SHIFT|CTRL', action = act.ActivateCopyMode },
+    { key = 'x', mods = 'SUPER', action = act.ActivateCopyMode },
+    { key = 'Z', mods = 'SHIFT|CTRL', action = act.TogglePaneZoomState },
+    { key = 'z', mods = 'SUPER', action = act.TogglePaneZoomState },
     { key = 'phys:Space', mods = 'SHIFT|CTRL', action = act.QuickSelect },
     { key = 'PageUp', mods = 'SHIFT', action = act.ScrollByPage(-1) },
-    { key = 'PageUp', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
     { key = 'PageUp', mods = 'SHIFT|CTRL', action = act.MoveTabRelative(-1) },
     { key = 'PageDown', mods = 'SHIFT', action = act.ScrollByPage(1) },
-    { key = 'PageDown', mods = 'CTRL', action = act.ActivateTabRelative(1) },
     { key = 'PageDown', mods = 'SHIFT|CTRL', action = act.MoveTabRelative(1) },
     { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Left' },
-    { key = 'LeftArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{ 'Left', 1 } },
     { key = 'RightArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Right' },
-    { key = 'RightArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{
-'Right', 1 } },
     { key = 'UpArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Up' },
-    { key = 'UpArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{ 'Up', 1 } },
     { key = 'DownArrow', mods = 'SHIFT|CTRL', action = act.ActivatePaneDirection 'Down' },
+    { key = 'LeftArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{ 'Left', 1 } },
+    { key = 'RightArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{ 'Right', 1 } },
+    { key = 'UpArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{ 'Up', 1 } },
     { key = 'DownArrow', mods = 'SHIFT|ALT|CTRL', action = act.AdjustPaneSize{ 'Down', 1 } },
-    { key = 'Insert', mods = 'SHIFT', action = act.PasteFrom 'PrimarySelection'
-},
+    { key = 'Insert', mods = 'SHIFT', action = act.PasteFrom 'PrimarySelection' },
     { key = 'Insert', mods = 'CTRL', action = act.CopyTo 'PrimarySelection' },
     { key = 'Copy', mods = 'NONE', action = act.CopyTo 'Clipboard' },
     { key = 'Paste', mods = 'NONE', action = act.PasteFrom 'Clipboard' },
-        -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
-    {
-        key = 'f',
-        mods = 'SHIFT|META',
-        action = wezterm.action.ToggleFullScreen,
-    },
     -- Ctrl+Shift+tで新しいタブを作成
     {
         key = 't',
         mods = 'CTRL|SHIFT',
         action = act.SpawnTab 'CurrentPaneDomain',
-    },
-    -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
-    {
-        key = 'd',
-        mods = 'SHIFT|CTRL',
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
     },
     -- Ctrl+左矢印でカーソルを前の単語に移動
     {
@@ -159,22 +100,20 @@ true, copy_to =  'ClipboardAndPrimarySelection' } },
     { key = "7", mods = "CTRL", action = act.ActivateTab(6) },
     { key = "8", mods = "CTRL", action = act.ActivateTab(7) },
     { key = "9", mods = "CTRL", action = act.ActivateTab(-1) },
-
     -- ¥でバックスラッシュ・ALT + ¥で¥
     { key = "¥", action = act.SendKey({ key = "\\" }) },
     { key = "¥", mods = "ALT", action = act.SendKey( {key = "¥" }) },
     -- ctrl+,で下にpane ctrl+.で右にpane
     { key = ",", mods = "CTRL", action = act.SplitVertical{ domain =  'CurrentPaneDomain' }},
     { key = ".", mods = "CTRL", action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' }},
-
+    -- LEADERの後にsでワークスペース切り替え
     {
     mods = 'LEADER',
     key = 's',
     action = act.ShowLauncherArgs { flags = 'WORKSPACES' , title = "Select workspace" },
     },
-
+    -- LEADER+SHFIT+sでワークスペース作成
     {
-    -- Create new workspace
     mods = 'LEADER|SHIFT',
     key = 'S',
     action = act.PromptInputLine {
@@ -188,9 +127,25 @@ true, copy_to =  'ClipboardAndPrimarySelection' } },
             pane
           )
         end
-        end),
+      end),
     },
-},
+    },
+    -- LEADER+SHIFT+$でワークスペース名変更
+    {
+    mods = 'LEADER|SHIFT',
+    key = '$',
+    action = act.PromptInputLine {
+      description = '(wezterm) Set workspace title:',
+      action = wezterm.action_callback(function(win,pane,line)
+        if line then
+          wezterm.mux.rename_workspace(
+            wezterm.mux.get_active_workspace(),
+            line
+          )
+        end
+      end),
+    },
+    },
   },
 
   key_tables = {
