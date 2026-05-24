fc886f963ed179ad73c78b463e7a66f6dd92bd4a
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-05-24 17:35:53 +0900

    refactor: alias.zsh に汎用 alias を追加

--- zsh/.zsh.d/alias.zsh
+++ zsh/.zsh.d/alias.zsh
@@ -1,19 +1,46 @@
 source ~/dotfiles/scripts/check_os.sh
 
+OS=$(check_os)
 
-    OS=$(check_os)
+if [[ $OS == "Cygwin" ]]; then
+    alias cursor-agent="${HOME}/AppData/Local/cursor-agent/agent.cmd"
+elif [[ $OS == "Mac" ]]; then
+    :
+elif [[ $OS == "CentOS" ]]; then
+    :
+elif [[ $OS == "Amazon Linux" ]]; then
+    :
+elif [[ $OS == "Ubuntu" ]]; then
+    :
+else
+    echo "Unsupported OS: $OS @alias.zsh"
+    return 1
+fi
 
-    if [[ $OS == "Cygwin" ]]; then
-	alias cursor-agent="${HOME}/AppData/Local/cursor-agent/agent.cmd"
-    elif [[ $OS == "Mac" ]]; then
-	:
-    elif [[ $OS == "CentOS" ]]; then
-        :
-    elif [[ $OS == "Amazon Linux" ]]; then
-        :
-    elif [[ $OS == "Ubuntu" ]]; then
-	:
-    else
-        echo "Unsupported OS: $OS"
-        return 1
-    fi
+# safty
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+# ls 系
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+
+# cd 系
+alias c~='cd ~'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+
+# git 系
+alias g='git'
+alias ga='git add'
+alias gd='git diff'
+alias gs='git status'
+alias gph='git push'
+alias gpl='git pull'
+alias gco='git checkout'
+alias gf='git fetch'
+alias gc='git commit'
+
