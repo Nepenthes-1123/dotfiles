f2bb9a245cf3b0dcee91f17679a901424d08902f
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-06-18 15:50:03 +0900

    feat: macOSでvueのシンタックスハイライトに成功

--- nvim/lua/plugins/config/treesitter.lua
+++ nvim/lua/plugins/config/treesitter.lua
@@ -1,24 +1,48 @@
-local setup = require('plugins.config.utils').setup
+local setup = require("plugins.config.utils").setup
 
 -- ── nvim-treesitter ───────────────────────────────────────────────────────────
-setup('nvim-treesitter.configs', function(m)
-    ---@diagnostic disable-next-line: missing-fields
-    m.setup({
-        ensure_installed      = {
-            'python', 'lua', 'c', 'cpp', 'vim', 'vimdoc',
-            'javascript', 'typescript', 'vue', 'json', 'jsonc',
-            'markdown', 'markdown_inline', 'latex',
-            'bash', 'yaml', 'toml', 'html', 'css',
-        },
-        highlight             = { enable = true, additional_vim_regex_highlighting = false },
-        indent                = { enable = true },
-        incremental_selection = {
-            enable  = true,
-            keymaps = {
-                init_selection   = '<C-Space>',
-                node_incremental = '<C-Space>',
-                node_decremental = '<BS>',
-            },
-        },
-    })
+setup("nvim-treesitter.configs", function(m)
+	---@diagnostic disable-next-line: missing-fields
+	m.setup({
+		ensure_installed = {
+			"python",
+			"lua",
+			"c",
+			"cpp",
+			"vim",
+			"vimdoc",
+			"javascript",
+			"typescript",
+			"vue",
+			"json",
+			"jsonc",
+			"markdown",
+			"markdown_inline",
+			"latex",
+			"bash",
+			"yaml",
+			"toml",
+			"html",
+			"css",
+			"pug",
+			"scss",
+		},
+		highlight = { enable = true, additional_vim_regex_highlighting = false },
+		indent = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-Space>",
+				node_incremental = "<C-Space>",
+				node_decremental = "<BS>",
+			},
+		},
+	})
 end)
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = "vue",
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
+})
