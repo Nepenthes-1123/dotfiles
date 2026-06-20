496b7f81bad3050b8e270bc68a9640221e61f1f2
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-06-21 07:47:23 +0900

    fix: コマンドラインの中央表示のために補完をあきらめる

--- nvim/lua/plugins/config/tiny-cmdline.lua
+++ nvim/lua/plugins/config/tiny-cmdline.lua
@@ -1,10 +1,10 @@
-local setup = require('plugins.config.utils').setup
+local setup = require("plugins.config.utils").setup
 
 -- ── tiny-cmdline ──────────────────────────────────────────────────────────────
-setup('tiny-cmdline', function(m)
-    m.setup({
-        -- 画面中央に表示 (デフォルト)
-        -- `/` (検索) や `:` (コマンド) が自動的にここで入力できるようになります
-        native_types = {},
-    })
+setup("tiny-cmdline", function(m)
+	m.setup({
+		-- 画面中央に表示 (デフォルト)
+		-- `/` (検索) や `:` (コマンド) が自動的にここで入力できるようになります
+		native_types = {},
+	})
 end)
