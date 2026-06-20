11d913c565778334839a6665b2b2c3be12e6d580
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-06-21 07:32:46 +0900

    feat: コマンドラインでの補完を追加

--- nvim/lua/plugins/config/blink.lua
+++ nvim/lua/plugins/config/blink.lua
@@ -12,6 +12,15 @@ setup("blink.cmp", function(m)
 		-- 補完ソース指定
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
+			cmdline = function()
+				local type = vim.fn.getcmdtype()
+				if type == "/" or type == "?" then
+					return { "buffer" }
+				end
+				if type == ":" then
+					return { "cmdline" }
+				end
+			end,
 		},
 		-- シグネチャーヘルプ(関数の引数ヒント)を有効化
 		signature = { enabled = true },
