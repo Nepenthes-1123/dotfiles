4b459aeb998d8aec27e0365d3954672f71e8a961
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-06-21 19:24:54 +0900

    feat: nvim-autopairsを有効化

--- nvim/lua/plugins/config/autopairs.lua
+++ nvim/lua/plugins/config/autopairs.lua
@@ -2,8 +2,13 @@ local setup = require("plugins.config.utils").setup
 
 setup("nvim-autopairs", function(m)
 	m.setup({
-
-		event = "InsertEnter",
-		config = true,
+		check_ts = true,
+		map_cr = false,
+		disable_filetype = {
+			"TelescopePrompt",
+			"snacks_picker_input",
+			"spectre_panel",
+			"octo",
+		},
 	})
 end)
