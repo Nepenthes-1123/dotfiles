ab6443a045ff5a3bedf77642b07bf45517b69380
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-06-27 15:56:33 +0900

    feat: explorerを開いた時もbufferlineが変わらないようにした

--- nvim/lua/plugins/config/bufferline.lua
+++ nvim/lua/plugins/config/bufferline.lua
@@ -24,14 +24,12 @@ setup("bufferline", function(m)
 			offsets = {
 				{
 					filetype = "NvimTree",
-					text = "File Explorer",
-					text_align = "left",
+					text = "",
 					separator = true,
 				},
 				{
-					filetype = "snacks_layout_box",
-					text = "File Explorer",
-					text_align = "left",
+					filetype = "snacks_explorer",
+					text = "",
 					separator = true,
 				},
 			},
