238df0bd5eca15684bfe52aba94f144368a0818a
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2026-07-03 20:53:47 +0900

    feat: gtodo-md.luaの追加

---
+++ nvim/lua/plugins/config/gtodo-md.lua
@@ -0,0 +1,13 @@
+-- =============================================================================
+-- lua/plugins/config/gtodo-md.lua  –  gtodo-md.nvim 設定
+-- =============================================================================
+
+local ok, gtodo_md = pcall(require, "gtodo-md")
+if not ok then
+	return
+end
+
+gtodo_md.setup({
+	use_default_keymaps = true,
+	picker = "auto",
+})
